4c1fb501405522cc37e3b717469213112fb55921
Lucy <lucybellott@gmail.com>
2023-06-26 18:14:18 -0400

    kth

--- hackerrank.js
+++ hackerrank.js
@@ -217,5 +217,62 @@ var addDigits = function(num) {
     let redTest = arr3.reduce((acc,curr)=> {
       return acc/curr
     })
-console.log(redTest)
+//console.log(redTest)
+
+//MERGE SORTED ARRAY 
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+var merge = function(nums1, m, nums2, n) {
+    // console.log(nums1, m, nums2, n)
+ 
+     
+     if(n === 0) {
+         return nums1
+     }
+ 
+     else {
+     
+     let i
+     const index = nums1.indexOf(0)
+     
+     for(i=0; i< nums1.length; i++){
+         nums1.splice(index,1)
+     }
+     console.log(nums1)
+ 
+    nums1.push(...nums2)
+ 
+     nums1.sort((a,b) => a-b)
+     return nums1
+     }
+ 
+ };
+
+// Kth Largest Element in an Array
+
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Can you solve it without sorting?
+
+//SORTING
+
+var findKthLargest = function(nums, k) {
+
+    let sorted = nums.sort((a,b) => a-b)
+  
+    let kIndex = sorted.length - k
+  
+    let result = nums[kIndex]
+  
+    return result
+      
+  };
+
+
 
