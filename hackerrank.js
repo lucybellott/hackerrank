ffced307298b0852f5be7fe1266f09183c5cd7af
Lucy <lucybellott@gmail.com>
2023-07-06 13:36:29 -0400

    diagonal difference

--- hackerrank.js
+++ hackerrank.js
@@ -482,5 +482,29 @@ while (s.indexOf(part) !== -1) {
     s = s.slice(0, index) + s.slice(index + part.length, s.length);
   }
 
+  ////////////////////////////////////////////////////////////////////////
+
+  //DIAGONAL DIFFERENCE
+
+  function diagonalDifference(arr) {
+    var n = arr.length; 
+    var d1 = 0;
+    var d2 = 0;
+  for(var i=0; i<n; i++){
+     for(var j=0; j<n; j++){
+       // finding the sum of primary diagonal
+         if(i === j) {
+           d1 += arr[i][j];
+         }
+       // finding the sum of secondary diagonal
+         if(i + j === n - 1){
+            d2 += arr[i][j];
+         }
+      }
+  }
+  return Math.abs(d1 - d2);
+}
+
+
 
 
