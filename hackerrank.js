b953cfe8d85cfccec9357d01e5d4fae0e78733b2
Lucy <lucybellott@gmail.com>
2023-07-09 16:54:05 -0400

    merge intervals

--- hackerrank.js
+++ hackerrank.js
@@ -599,5 +599,31 @@ function isPalindrome(s) {
         
     };
 
+    ////////////////////////////////////////////////////////////////////////////
+
+    //MERGE INTERVALS
+
+    var merge = function(intervals) {
+        intervals.sort((a, b) => a[0] - b[0]);
+    
+        let merged = [];
+        let currentInterval = intervals[0];
+    
+        for (let i = 1; i < intervals.length; i++) {
+            let nextInterval = intervals[i];
+    
+            if (currentInterval[1] >= nextInterval[0]) {
+                currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
+            } else {
+                merged.push(currentInterval);
+                currentInterval = nextInterval;
+            }
+        }
+    
+        merged.push(currentInterval);
+    
+        return merged;
+    };
+
 
 
