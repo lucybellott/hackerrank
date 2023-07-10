52d1fe5b12abcc2f11833802f7f2217742bc2865
Lucy <lucybellott@gmail.com>
2023-07-10 18:29:06 -0400

    validate brackets

--- hackerrank.js
+++ hackerrank.js
@@ -742,6 +742,30 @@ function isValid(s) {
     return stack.length === 0;
   }
 
+  //or
+
+  var isValid3 = function(s) {
+    if (s.length % 2 !== 0 ) return false;
+    let str = s.trim().split('');
+    let temp = [];
+    for(let i of str){
+        if(i == '(' || i=='{' || i=='['){
+            temp.push(i);
+        }
+        if(i==')'){
+            if(temp.pop() != '(') return false;
+        }
+        if(i=='}'){
+            if(temp.pop() != '{') return false;
+        }
+        if(i==']'){
+            if(temp.pop() != '[') return false;
+        }
+    };
+    return temp.length > 0 ? false: true;
+};
+
+
   ///////////////////////////
   
   
