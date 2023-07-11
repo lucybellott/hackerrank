49cce077512ab612f10876dc1267407c3c59398a
Lucy <lucybellott@gmail.com>
2023-07-11 15:44:18 -0400

    word break

--- hackerrank.js
+++ hackerrank.js
@@ -769,7 +769,31 @@ function isValid(s) {
   ///////////////////////////
   
   
-  
+  //WORD BREAK/DICT
+
+  var wordBreak = function(s, wordDict) {
+    const words = new Set(wordDict)
+       const starts = new Set([0])
+       
+       for(const start of starts){
+           for(const word of words){
+               const end = start + word.length
+               // we are slicing the word from s and check if that sliced word is exist in wordDict
+               // it could be the current word or it could be the other word which has same length of current word
+               // either way, we admit that the current starting index to the index of word length exist
+               // so now we want to add the next starting index which is 'end'
+               // we repeat this process until the last index of s
+               // if we reached to last index of s, that means that we checked that every sliced word exist in wordDict
+               // until the last index of s
+               // this will conclude that the s has every word from wordDict
+               if(words.has(s.slice(start, end))) starts.add(end)
+           }
+       }
+       return starts.has(s.length)
+   
+       
+   
+   };
   
   
   
