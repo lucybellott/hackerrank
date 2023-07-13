b4836f6daa5ae06bb4b12967198f4f9262863872
Lucy <lucybellott@gmail.com>
2023-07-13 16:03:05 -0400

    remove element

--- hackerrank.js
+++ hackerrank.js
@@ -900,7 +900,16 @@ var mergeAlternately = function(word1, word2) {
     return result
 
 };
-    
+    /////////////////////////////
+
+    //REMOVE ELEMENT
+
+    var removeElement = function(nums, val) {
+        while(nums.indexOf(val) !== -1) {
+            nums.splice(nums.indexOf(val), 1)
+        }
+        return nums.length;
+    };
   
   
   
