b9a5ecaac03eed59183183ab0d0ab24e77ffc4a7
Lucy <lucybellott@gmail.com>
2023-07-17 13:44:59 -0400

    CAN PLACE FLOWERS

--- hackerrank.js
+++ hackerrank.js
@@ -1128,6 +1128,33 @@ function findCommonDivisor(str1, str2) {
 };
 ///////////////////////////////////
 
+//CAN PLACE FLOWERS
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty,
+//  and an integer n, return true if n new flowers 
+// can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+var canPlaceFlowers = function(flowerbed, n) {
+    //[1,0,0,0,1]
+    let freeSpaces=[]
+
+    for(i=1;i<flowerbed.length-1; i++){
+      if(flowerbed[i+1]===0 && flowerbed[i-1]===0){
+        freeSpaces.push(i)
+      }
+    }
+    //console.log(freeSpaces)
+    if(freeSpaces.length< n){
+      return false
+    }
+    else{
+      return true
+    }
+    
+};
+
+////////////////////////////////////////////////////////////////
+
 
 
   
