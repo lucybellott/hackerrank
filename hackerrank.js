58b5b472ba3984024f19ea94227bd7c90a3be3a7
Lucy <lucybellott@gmail.com>
2023-07-16 22:16:59 -0400

    REDUCE TO ZERO

--- hackerrank.js
+++ hackerrank.js
@@ -1003,6 +1003,40 @@ var mergeAlternately = function(word1, word2) {
         }
         };
 
+        //////////////////////////////
+
+  //NUMBER OF STEPS TO REDUCE A NUMBER TO ZERO
+  
+//   Input: num = 14
+// Output: 6
+// Explanation: 
+// Step 1) 14 is even; divide by 2 and obtain 7. 
+// Step 2) 7 is odd; subtract 1 and obtain 6.
+// Step 3) 6 is even; divide by 2 and obtain 3. 
+// Step 4) 3 is odd; subtract 1 and obtain 2. 
+// Step 5) 2 is even; divide by 2 and obtain 1. 
+// Step 6) 1 is odd; subtract 1 and obtain 0.
+
+
+var numberOfSteps = function(num) {
+    
+    let counter = 0
+
+    while(num > 0){
+        if(num%2 ===0){
+            num = num/2
+            counter++
+        }
+        else {
+           num= num -1
+            counter++
+        }
+
+    }
+return counter
+
+};
+
   
   
   
