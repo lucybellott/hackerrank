b8ec8ca397eed6829c236d8da29100a5bec6053a
Lucy <lucybellott@gmail.com>
2023-07-17 12:25:10 -0400

    GREATEST COMMON DIVISOR OF STRS

--- hackerrank.js
+++ hackerrank.js
@@ -1086,6 +1086,26 @@ function findCommonDivisor(str1, str2) {
  // console.log(findCommonDivisor("hello", "hellohellohello")); // Output: "hello"
   //console.log(findCommonDivisor("abcdabcd", "abcdabcdabcdabcdabcd")); // Output: "abcd"
   //console.log(findCommonDivisor("abababab", "abab")); // Output: "ab"
+
+  //OR
+
+  var gcdOfStrings2 = function(str1, str2) {
+    
+    // If the concatenated strings are not equal,
+   // then a common divisor cannot be found
+   if (str1 + str2 !== str2 + str1) {
+     return '';
+   }
+ 
+   // Calculate the greatest common divisor of the string lengths
+   const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+   const len = gcd(str1.length, str2.length);
+ 
+   // Return the substring that is a common divisor
+   return str1.substring(0, len);
+   
+   
+ };
   
   
   
