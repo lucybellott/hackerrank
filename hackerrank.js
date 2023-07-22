ea1e9ab016c6e509a6b30efd36aa3e25f28301fd
Lucy <lucybellott@gmail.com>
2023-07-22 16:39:14 -0400

    PRODUCT OF ARRAY EXCEPT SELF 2

--- hackerrank.js
+++ hackerrank.js
@@ -1417,3 +1417,22 @@ var productExceptSelf = function (nums) {
     }
     return nums
 };
+//or
+var productExceptSelf2 = function (nums) {
+    const res = [];
+    let product = 1;
+  
+    for (let i = 0; i < nums.length; i++) {
+      res.push(product);
+      product *= nums[i];
+    }
+  
+    product = 1;
+  
+    for (let j = nums.length - 1; j >= 0; j--) {
+      res[j] *= product;
+      product *= nums[j];
+    }
+  
+    return res;
+  };
