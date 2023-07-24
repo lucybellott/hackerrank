9dbbc551741bb904d8d88361eaa9105ea8d614dd
Lucy <lucybellott@gmail.com>
2023-07-24 18:54:07 -0400

    is subsequence

--- hackerrank.js
+++ hackerrank.js
@@ -1464,3 +1464,18 @@ var productExceptSelf2 = function (nums) {
     return false;
   
   }
+
+  //or
+
+  var isSubsequence2 = function(s, t) {
+    let currentIndex = -1;
+   for (let i = 0; i < s.length; i++) {
+     const target = t.indexOf(s[i], currentIndex + 1);
+     if (target > currentIndex) {
+       currentIndex = target;
+     } else {
+       return false;
+     }
+   }
+   return true;
+ };
