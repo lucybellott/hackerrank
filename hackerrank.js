ea4124f91bcbd553598894b69810521928036a6c
Lucy <lucybellott@gmail.com>
2023-07-27 14:50:19 -0400

    increasing triplet

--- hackerrank.js
+++ hackerrank.js
@@ -1543,4 +1543,19 @@ var increasingTriplet = function(nums) {
 };
 
 //or
+var increasingTriplet2 = function(nums) {
+    const temp = [Infinity, Infinity];
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > temp[1] && temp[1] > temp[0]) return true;
+        
+        if (nums[i] > temp[0]) {
+            temp[1] = nums[i];
+        } else {
+            temp[0] = nums[i];
+        }
+    }
+    
+    return false;
+}; 
 
