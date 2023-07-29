027fa9519edd9864019c4bdd32f360e66f42320b
Lucy <lucybellott@gmail.com>
2023-07-29 19:56:29 -0400

    STRING COMPRESSION

--- hackerrank.js
+++ hackerrank.js
@@ -1559,3 +1559,23 @@ var increasingTriplet2 = function(nums) {
     return false;
 }; 
 
+//STRING COMPRESSION
+var compress = function(chars) {
+    let i = 0;
+    let j = 0;
+    while (j < chars.length) {
+        let count = 0;
+        let curr = chars[j];
+        while (j < chars.length && chars[j] === curr) {
+            j++;
+            count++;
+        }
+        chars[i++] = curr;
+        if (count > 1) {
+            for (let digit of count.toString()) {
+                chars[i++] = digit;
+            }
+        }
+    }
+    return i;
+};
