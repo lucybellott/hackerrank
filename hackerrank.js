d5fe8051709062da24ac4c900c657df57d195bde
Lucy <lucybellott@gmail.com>
2023-07-31 16:19:49 -0400

    most water

--- hackerrank.js
+++ hackerrank.js
@@ -1616,3 +1616,23 @@ var compress2 = function(chars) {
 
     return idx;
 };
+
+//CONTAINER WITH MOST WATER
+var maxArea = function (height) {
+    var leftIndex = 0;
+    var rightIndex = height.length - 1;
+    var maxStoredWater = 0;
+
+    while (leftIndex < rightIndex) {
+        const leftHeight = height[leftIndex];
+        const rightHeight = height[rightIndex];
+        const width = rightIndex - leftIndex;
+        const smallerHeight = leftHeight < rightHeight ? leftHeight : rightHeight;
+        const storedWater = width * smallerHeight;
+
+        maxStoredWater = storedWater > maxStoredWater ? storedWater : maxStoredWater;
+        smallerHeight == leftHeight ? leftIndex++ : rightIndex--;
+    }
+
+    return maxStoredWater;
+};
